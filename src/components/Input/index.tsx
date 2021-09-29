f4856a5ba5ec14f260400d216a26803fcfa1b49c
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-29 14:50:53 -0300

    feat: add error styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,13 +4,15 @@ import styles from "./styles.module.scss";
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   id: string;
+  error?: string;
 }
 
-export const Input = ({ label, id, ...props }: InputProps) => {
+export const Input = ({ label, id, error, ...props }: InputProps) => {
   return (
     <div className={styles["form-group"]}>
       <label htmlFor={id}>{label}</label>
       <input id={id} className={styles["form-control"]} {...props} />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
