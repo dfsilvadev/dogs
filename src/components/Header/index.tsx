561d32ffc72f079c574257a794a56e969a773318
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-29 10:16:49 -0300

    feat: definindo a estrutura do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
+
 import styles from "./styles.module.scss";
 
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className={styles.header}>
+      <nav className="container">
+        <Link to="/" className={styles.logo}>
+          <img
+            src={"/images/logo.svg"}
+            alt="Logotipo dogs - um desenho de cachorro sorrindo"
+            aria-label="Logotipo dogs - um desenho de cachorro sorrindo"
+          />
+        </Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </header>
   );
 };
