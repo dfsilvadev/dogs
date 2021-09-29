aee8f9bbe1b9404f30e3d05e6e7ea8870749dd44
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-29 14:33:47 -0300

    feat: definindo as rotas de login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+
 import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="login/" element={<Login />}>
+        <Route path="cadastrar" element={<p>Cadastrar</p>} />
+        <Route path="esqueci-a-senha" element={<p>Esqueci minha senha</p>} />
+      </Route>
     </Routes>
   );
 };
