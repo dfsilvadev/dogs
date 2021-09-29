58e14d4beb459eded5f5497feca0b5e873d8ae81
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-29 14:34:06 -0300

    feat: button default

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes, FormEvent, ReactNode } from "react";
+
+import styles from "./styles.module.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  handleSignIn?: (event: FormEvent<HTMLButtonElement>) => void;
+}
+
+export function Button({ children, handleSignIn, ...props }: ButtonProps) {
+  return (
+    <button onClick={handleSignIn} {...props} className={styles.fill}>
+      {children}
+    </button>
+  );
+}
