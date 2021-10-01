905fdee1606ee6965a0df1f4acf1d5acfb06e18f
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-01 17:24:24 -0300

    feat: buscando dados do usuário com o token

--- src/contexts/useAuth.tsx
+++ src/contexts/useAuth.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
 import { useCookies } from "../hooks/useCookies";
 import { api } from "../services/api";
 
@@ -7,6 +8,7 @@ interface AuthContextProviderProps {
 }
 
 interface AuthContextData {
+  user: User;
   signIn: (credentials: Credentials) => Promise<void>;
   getUserData: (token: string) => Promise<void>;
 }
@@ -16,18 +18,26 @@ interface Credentials {
   password: string;
 }
 
+interface User {
+  id: string;
+  email: string;
+  nome: string;
+  username: string;
+}
+
 export const UseAuthContext = createContext({} as AuthContextData);
 
 export const UseAuthContextProvider = ({
   children,
 }: AuthContextProviderProps) => {
-  const { cookies } = useCookies();
+  const [user, setUser] = useState<User>({} as User);
+  const { cookies, token } = useCookies();
 
   async function signIn({ username, password }: Credentials) {
     try {
       const response = await api
         .post(
-          "json/jwt-auth/v1/token",
+          "jwt-auth/v1/token",
           { username, password },
           {
             headers: {
@@ -49,20 +59,38 @@ export const UseAuthContextProvider = ({
 
   async function getUserData(token: string) {
     try {
-      const response = api.get("api/user", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const data = await api
+        .get("api/user", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => response.data);
 
-      console.log(response);
+      return data;
     } catch (err) {
       console.log(err);
     }
   }
 
+  useEffect(() => {
+    if (token) {
+      (async () => {
+        const data = await getUserData(token);
+        const { id, nome, username, email } = data;
+
+        setUser({
+          id,
+          nome,
+          email,
+          username,
+        });
+      })();
+    }
+  }, [token]);
+
   return (
-    <UseAuthContext.Provider value={{ signIn, getUserData }}>
+    <UseAuthContext.Provider value={{ user, getUserData, signIn }}>
       {children}
     </UseAuthContext.Provider>
   );
