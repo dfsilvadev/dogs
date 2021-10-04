fa0f0c92118e89e87d3ed2b0af139566e02f1144
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-04 15:22:38 -0300

    feat: definindo os estilos da page Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,59 +1,18 @@
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+import { Route, Routes } from "react-router-dom";
+import { SignInForm } from "../../components/SignInForm";
 
-import { useAuth } from "../../hooks/useAuth";
-
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
-import { useNavigate } from "react-router";
-
-interface Credentials {
-  username: string;
-  password: string;
-}
-
-const createUserFormSchema = yup.object({
-  username: yup.string().required("E-mail obrigatório."),
-  password: yup
-    .string()
-    .required("Senha obrigatória.")
-    .min(6, "A senha deve conter 6 ou mais caracteres"),
-});
+import styles from "../../styles/pages/Login.module.scss";
 
 export const Login = () => {
-  const navigate = useNavigate();
-  const { signIn } = useAuth();
-  const { register, handleSubmit, formState } = useForm({
-    resolver: yupResolver(createUserFormSchema),
-  });
-
-  const { errors } = formState;
-
-  async function handleSignIn(credentials: Credentials) {
-    await signIn(credentials);
-    navigate("/");
-  }
-
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit(handleSignIn)}>
-        <Input
-          type="text"
-          label="Usuário"
-          id="username"
-          error={errors.username}
-          {...register("username")}
-        />
-        <Input
-          type="password"
-          label="Senha"
-          id="password"
-          error={errors.password}
-          {...register("password")}
-        />
-        <Button type="submit">Entrar</Button>
-      </form>
-    </div>
+    <section className={styles.wrapper}>
+      <div className={styles.form}>
+        <Routes>
+          <Route path="/" element={<SignInForm />} />
+          <Route path="cadastrar" element={<p>Cadastrar</p>} />
+          <Route path="esqueci-a-senha" element={<p>Esqueci minha senha</p>} />
+        </Routes>
+      </div>
+    </section>
   );
 };
