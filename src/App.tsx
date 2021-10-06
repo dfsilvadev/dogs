e30e75c0a914cb2e78c45921b06e43dc75b04e7b
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-06 16:46:49 -0300

    refatoring: refatorando o código

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import { UseAuthContextProvider } from "./contexts/useAuth";
 import { MyRoutes as Routes } from "./routes";
-
-import { Layout } from "./components/Layout";
 
 import "./styles/global.scss";
 
 function App() {
   return (
     <BrowserRouter>
-      <UseAuthContextProvider>
-        <Layout>
-          <Routes />
-          <ToastContainer autoClose={3000} />
-        </Layout>
-      </UseAuthContextProvider>
+      <Routes />
+      <ToastContainer autoClose={3000} />
     </BrowserRouter>
   );
 }
