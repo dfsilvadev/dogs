4b3356d29918c14a7fdcb879b918f96fd7001d41
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-07 09:49:50 -0300

    feat: add useRouteMath

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 
 import { SignInForm } from "../../components/SignInForm";
 import { SignUpForm } from "../../components/SignUpForm";
@@ -6,14 +6,18 @@ import { SignUpForm } from "../../components/SignUpForm";
 import styles from "../../styles/pages/Login.module.scss";
 
 export const Login = () => {
+  const { path } = useRouteMatch();
   return (
     <section className={styles.wrapper}>
       <div className={styles.form}>
-        <Routes>
-          <Route path="/" element={<SignInForm />} />
-          <Route path="esqueci-a-senha" element={<p>Esqueci minha senha</p>} />
-          <Route path="cadastrar" element={<SignUpForm />} />
-        </Routes>
+        <Switch>
+          <Route exact path={path} component={SignInForm} />
+          {/* <Route
+            path="esqueci-a-senha"
+            component={<p>Esqueci minha senha</p>}
+          /> */}
+          <Route path={`${path}/cadastrar`} component={SignUpForm} />
+        </Switch>
       </div>
     </section>
   );
