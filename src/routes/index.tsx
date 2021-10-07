2a564a5af64a2f6c0d364c131e6d2e5a9c539fdc
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-07 09:50:36 -0300

    feat: import UseAuthContextProvider e Layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
+import { UseAuthContextProvider } from "../contexts/useAuth";
+
+import { Layout } from "../components/Layout";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { Conta } from "../pages/Account";
+import { PrivateRoute } from "./private";
 
 export const MyRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="login/*" element={<Login />} />
-    </Routes>
+    <UseAuthContextProvider>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/minha-conta" component={Conta} />
+        </Switch>
+      </Layout>
+    </UseAuthContextProvider>
   );
 };
