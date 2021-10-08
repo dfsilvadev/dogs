a07997346a364976f61a6c7fb3da617019da4549
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-08 12:45:13 -0300

    feat: definindo as rotas de login

---
+++ src/routes/login.tsx
@@ -0,0 +1,18 @@
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+
+import { SignInForm } from "../components/SignInForm";
+import { SignUpForm } from "../components/SignUpForm";
+
+export const LoginRoutes = () => {
+  const { path } = useRouteMatch();
+  return (
+    <Switch>
+      <Route exact path={path} component={SignInForm} />
+      {/* <Route
+            path="esqueci-a-senha"
+            component={<p>Esqueci minha senha</p>}
+          /> */}
+      <Route path={`${path}/cadastrar`} component={SignUpForm} />
+    </Switch>
+  );
+};
