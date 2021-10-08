4d0398074bd7db5721f30900109dcc1d089c1270
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-08 11:19:15 -0300

    feat: hook para validar screen mobile

---
+++ src/hooks/useMediaMatch.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const useMediaMatch = (screenValue: string) => {
+  const [match, setMatch] = useState(false);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = matchMedia(screenValue);
+      setMatch(matches);
+    }
+
+    window.addEventListener("resize", changeMatch);
+
+    return () => {
+      window.removeEventListener("resize", changeMatch);
+    };
+  }, [screenValue]);
+
+  return { match };
+};
