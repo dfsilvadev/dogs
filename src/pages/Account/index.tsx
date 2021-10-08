4ad2130b6b93e2d759b0aa4a11b57ae65dd8d53a
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-08 11:18:51 -0300

    feat: add AccountNavHeader

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+
 import { AccountNavHeader } from "../../components/AccountNavHeader";
+import { converteToArray } from "../../utils/converteToArray";
+import { removeEmptyElements } from "../../utils/removeEmptyElements";
 
 import styles from "../../styles/pages/Account.module.scss";
 
 export const Account = () => {
+  const [title, setTitle] = useState("");
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const paths = converteToArray(pathname, "/");
+    const newPaths = removeEmptyElements(paths);
+
+    if (newPaths.length <= 1) {
+      setTitle("Minha Conta");
+      return;
+    }
+
+    if (newPaths.length > 1 && newPaths.includes("estatisticas")) {
+      setTitle("Estatisticas");
+      return;
+    }
+
+    if (newPaths.length > 1 && newPaths.includes("novo-post")) {
+      setTitle("Nova Postagem");
+      return;
+    }
+  }, [pathname]);
+
   return (
     <section className={styles.wrapper}>
       <div className="container">
-        <AccountNavHeader />
+        <AccountNavHeader title={title} />
       </div>
     </section>
   );
