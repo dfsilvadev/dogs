eafe1a4f5db769dc3f1b414b06c6be5d5d63c88f
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-13 15:37:20 -0300

    feat: exibindo os posts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
+import { PostSection } from "../../components/PostSection";
 import styles from "../../styles/pages/Home.module.scss";
 
 export const Home = () => {
   return (
     <section className={styles["home-wrapper"]}>
       <div className="container">
-        <h1>Home</h1>
+        <PostSection />
       </div>
     </section>
   );
