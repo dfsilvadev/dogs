4f93af1b49869cd5f26d9d3ffcafeb4a6b51d0f7
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-13 15:39:26 -0300

    feat: add feed as rotas

--- src/routes/account.tsx
+++ src/routes/account.tsx
@@ -3,17 +3,14 @@ import { Switch, useRouteMatch } from "react-router-dom";
 import { PrivateRoute } from "./private";
 
 import { PhotoPost } from "../pages/PhotoPost";
+import { Feed } from "../pages/Feed";
 
 export const AccountRoutes = () => {
   const { path } = useRouteMatch();
 
   return (
     <Switch>
-      <PrivateRoute
-        exact
-        path={path}
-        component={() => <h1>Minhas postagens</h1>}
-      />
+      <PrivateRoute exact path={path} component={Feed} />
       <PrivateRoute
         path={`${path}/estatisticas`}
         component={() => <h1>Estatísticas</h1>}
